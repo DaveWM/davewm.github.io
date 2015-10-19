8ff34113e987a40c71d6ad45ff26db28bf0cb521
David Martin <dwmartin41@gmail.com>
2015-10-19 23:51:27 +0100

    first version of summary page

---
+++ src/clj/material_ui/macros.clj
@@ -0,0 +1,20 @@
+; shamelessly stolen from https://github.com/tuhlmann/reagent-material
+
+(ns material-ui.macros)
+
+(def material-tags
+  '[AppBar
+    Avatar
+    Card
+    CardHeader
+    CardMedia
+    CardText
+    CardTitle
+    FlatButton
+    FontIcon])
+
+(defn material-ui-react-import [tname]
+  `(def ~tname (reagent.core/adapt-react-class (aget js/MaterialUI ~(name tname)))))
+
+(defmacro export-material-ui-react-classes []
+  `(do ~@(map material-ui-react-import material-tags)))
