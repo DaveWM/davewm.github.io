1439a4bd62eee1708794ec745bc1abd4bb489077
David Martin <dwmartin41@gmail.com>
2015-11-05 22:47:23 +0000

    got figwheel working

--- project.clj
+++ project.clj
@@ -5,28 +5,31 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 ;[com.cemerick/piggieback "0.2.1"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [weasel "0.7.0" :exclusions [org.clojure/clojurescript]]
                  [org.clojure/clojurescript "1.7.145"]
                  [reagent "0.5.1" :exclusions [cljsjs/react]]
                  [cljs-ajax "0.3.14"]
                  [cljsjs/react-router "0.13.2-0" :exclusions [cljsjs/react]]
                  ]
   :clean-targets ^{:protect false} ["dist/js" "target"]
-  :plugins [[cider/cider-nrepl "0.9.1"] [lein-cljsbuild "1.1.0"]]
+  :plugins [[cider/cider-nrepl "0.9.1"]
+            [lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.1"]
+            ]
   :cljsbuild {:builds
               [{
                 ;; CLJS source code path
                 :source-paths ["src/cljs/"]
                 :source-map true
-
+                :figwheel true
+                           
                 ;; Google Closure (CLS) options configuration
                 :compiler {;; CLS generated JS script filename
-                           :output-to "dist/js/app.js"
+                           :output-to "dist/app.js"
+
+                           :main "app"
 
                            ;; minimal JS optimization directive
-                           :optimizations :whitespace
+                           ;:optimizations :none
 
                            ;; generated JS code prettyfication
                            :pretty-print true
--- src/cljs/connect.cljs
+++
@@ -1,4 +0,0 @@
-(ns my.core.cljs
- (:require [weasel.repl :as repl]))
- ;(repl/connect "ws://localhost:9001")
-
