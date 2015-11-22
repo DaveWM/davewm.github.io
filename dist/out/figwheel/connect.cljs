605d6d3ffb3011b0de4774c5f8d0b54a155604c5
David Martin <dwmartin41@gmail.com>
2015-11-22 00:00:58 +0000

    removed figwheel build files from repo

--- dist/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [app]))
-(figwheel.client/start {:build-id "build_needs_id_7045", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
--- dist/out/figwheel/connect.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.7.145 {}
-goog.provide('figwheel.connect');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-goog.require('app');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"build-id","build-id",1642831089),"build_needs_id_7045",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
-
-//# sourceMappingURL=connect.js.map
