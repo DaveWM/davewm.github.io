e088b5352c3a0cbca45ef8651c7b98e9b4503706
David Martin <dwmartin41@gmail.com>
2016-02-22 23:39:23 +0000

    added enter animation to tech page

--- src/cljs/technologies/page.cljs
+++ src/cljs/technologies/page.cljs
@@ -6,9 +6,16 @@
   (:require-macros [cljs.core :refer [this-as]]))
 (enable-console-print!)
 
+(def css-transition-group
+  (r/adapt-react-class js/React.addons.CSSTransitionGroup))
 (def chart-size 100)
 
 (defn page []
-  [Paper {:class "tech-chart"}
-   [:p "The size of each bubble represents the experience I have with that technology."]
-   [chart {:chart-size chart-size :technologies data/data}]])
+  [:div.row.middle-xs
+   [css-transition-group {:transition-name "card"
+                          :transition-appear true
+                          :class "col-xs-12 card-container"}
+    [:div {:class "col-xs-12"}
+     [Paper {:class "tech-chart"}
+      [:p "The size of each bubble represents the experience I have with that technology."]
+      [chart {:chart-size chart-size :technologies data/data}]]]]])
